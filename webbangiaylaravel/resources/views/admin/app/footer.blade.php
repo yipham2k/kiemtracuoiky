23de3cb331f8c045a96a7ba0c530f9e9f8d3972d
yipham2k <anhduyphamhoang2000@gmail.com>
2025-04-15 23:03:30 +0700

    kết thúc moni

---
+++ webbangiaylaravel/resources/views/admin/app/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            © 2025 Bản quyền thuộc về anhduyphamhoang2000:
+            <a class="text-info" href="/ ">SHOESVN - Anh Duy</a>
+        </div>
+    </div>
+</footer>
